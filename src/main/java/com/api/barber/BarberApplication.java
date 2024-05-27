122c358f734b37540fefbf6f957ba0d0c1cda0d9
antmarques <antonio5784881@gmail.com>
2024-05-27 16:43:50 -0300

    Project Created

---
+++ src/main/java/com/api/barber/BarberApplication.java
@@ -0,0 +1,13 @@
+package com.api.barber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/barber/BarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.barber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
